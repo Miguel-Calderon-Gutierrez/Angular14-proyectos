960ef8c2b759d7bd8cd91cf762f1583e9ff37f8f
Miguel Calderon <miguelcalderon.dev@gmail.com>
2025-02-09 14:52:00 -0500

    add example ngFor and ngIf

--- directivas/src/app/app.component.ts
+++ directivas/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'directivas';
+  lista: any[] = [
+    { Nombre: 'Miguel', Estado: true },
+    { Nombre: 'Angel', Estado: false },
+    { Nombre: 'Diana', Estado: true },
+  ];
+
+  verContenido = false;
+  mostrar():void{
+    this.verContenido = !this.verContenido;
+  }
 }
